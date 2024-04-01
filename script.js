92f7ec1891b651d76998eeb94b09d1f986a79bcf
Saitheja komalla <komallasaiteja@gmail.com>
2024-04-01 13:46:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById("dropdownBtn").addEventListener("click", function() {
-    var dropdownContent = document.getElementById("dropdownContent");
-    if (dropdownContent.style.display === "none") {
-        dropdownContent.style.display = "block"; // Show content
-    } else {
-        dropdownContent.style.display = "none"; // Hide content
-    }
-});
+// document.getElementById("dropdownBtn").addEventListener("click", function() {
+//     var dropdownContent = document.getElementById("dropdownContent");
+//     if (dropdownContent.style.display === "none") {
+//         dropdownContent.style.display = "block"; // Show content
+//     } else {
+//         dropdownContent.style.display = "none"; // Hide content
+//     }
+// });
 // document.addEventListener("DOMContentLoaded", function() {
 //     // Wait for the DOM to be fully loaded
 //     document.getElementById("dropdownBtn").addEventListener("click", function() {
@@ -17,14 +17,14 @@ document.getElementById("dropdownBtn").addEventListener("click", function() {
 //         }
 //     });
 // });
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Wait for the DOM to be fully loaded
-//     document.getElementById("dropdownBtn").addEventListener("click", function() {
-//         var dropdownContent = document.getElementById("dropdownContent");
-//         if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
-//             dropdownContent.style.display = "block"; // Show content
-//         } else {
-//             dropdownContent.style.display = "none"; // Hide content
-//         }
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    // Wait for the DOM to be fully loaded
+    document.getElementById("dropdownBtn").addEventListener("click", function() {
+        var dropdownContent = document.getElementById("dropdownContent");
+        if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
+            dropdownContent.style.display = "block"; // Show content
+        } else {
+            dropdownContent.style.display = "none"; // Hide content
+        }
+    });
+});
